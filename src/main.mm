96258ae0de3741b51ab39a6671feedc7bfa49890
celiocasttro <celiocastroreal@gmail.com>
2026-07-30 18:21:15 -0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -5,20 +5,7 @@ static ImGuiDrawView *menuView = nil;
 
 __attribute__((constructor))
 static void initialize() {
-    // Executa assim que o binário é carregado
     dispatch_async(dispatch_get_main_queue(), ^{
-        // Fica checando a janela ativa a cada 1 segundo até o jogo carregar a interface gráfica
-        __block __weak id observer = [[NSNotificationCenter defaultCenter] addObserverForName:UIWindowDidBecomeKeyNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
-            UIWindow *window = note.object;
-            if (window && !menuView) {
-                menuView = [[ImGuiDrawView alloc] initWithFrame:window.bounds];
-                menuView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-                [window addSubview:menuView];
-                [window bringSubviewToFront:menuView];
-            }
-        }];
-        
-        // Tentativa imediata caso a janela já exista
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
             if (keyWindow && !menuView) {
